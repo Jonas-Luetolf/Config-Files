f370afd35dc0de08545c88390e32be894b1c095a
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-07-22 18:28:21 +0200

    add alacritty dunst & update xmonad

--- Jonas-Luetolf-xmonad.hs
+++ Jonas-Luetolf-xmonad.hs
@@ -15,8 +15,8 @@ import qualified Data.Map        as M
 
 import Graphics.X11.ExtraTypes.XF86
 
--- The preferred terminal program, which is used in a binding below and by
--- certain contrib modules.
+import XMonad.Actions.GridSelect
+import XMonad.Actions.NoBorders
 --
 myTerminal      = "alacritty"
 
@@ -26,96 +26,117 @@ myFocusFollowsMouse = True
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
-myBorderWidth   = 3
-myXmobarrc = "~/.config/xmobar/xmobarrc"
+myBorderWidth   = 4
+--myXmobarrc = "~/.config/xmobar/xmobarrc"
+--myConkyrc = "~/.conky/nord-01.conkyrc"
 myModMask       = mod1Mask
 
 myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 
 -- Border colors
-myNormalBorderColor  = "#dddddd"
-myFocusedBorderColor = "#ff0000"
+myNormalBorderColor  = "#EBCB8B"
+myFocusedBorderColor = "#88C0D0"
 
+toggleFull = withFocused (\windowId -> do    
+{       
+   floats <- gets (W.floating . windowset);        
+   if windowId `M.member` floats        
+   then do 
+       withFocused $ toggleBorder           
+       withFocused $ windows . W.sink
+   
+   else do 
+       withFocused $ toggleBorder           
+       withFocused $  windows . (flip W.float $ W.RationalRect 0 0 1 1)    }    )
 ------------------------------------------------------------------------
 -- Key bindings
-
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
-    -- launch a terminal
+-- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
-    -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run")
 
-    -- close focused window
+    ,((modm,               xK_f     ), toggleFull)
+
+-- launch rofi
+    , ((modm,               xK_p     ), spawn "rofi -show drun")
+    , ((modm .|. shiftMask, xK_p     ), spawn "rofi -show run")
+
+    , ((modm .|. shiftMask, xK_t     ), spawn "killall trayer")
+
+-- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
 
-     -- Rotate through the available layout algorithms
+-- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
 
-    --  Reset the layouts on the current workspace to default
+--  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
 
-    -- Resize viewed windows to the correct size
+-- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
 
-    -- Move focus to the next window
+-- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
 
-    -- Move focus to the next window
+-- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
 
-    -- Move focus to the previous window
+-- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp  )
 
-    -- Move focus to the master window
+-- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
 
-    -- Swap the focused window and the master window
+-- Swap the focused window and the master window
     , ((modm,               xK_Return), windows W.swapMaster)
 
-    -- Swap the focused window with the next window
+-- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
 
-    -- Swap the focused window with the previous window
+-- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
 
-    -- Shrink the master area
+-- Shrink the master area
     , ((modm,               xK_h     ), sendMessage Shrink)
 
-    -- Expand the master area
+-- Expand the master area
     , ((modm,               xK_l     ), sendMessage Expand)
 
-    -- Push window back into tiling
+-- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
 
-    -- Increment the number of windows in the master area
+-- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
 
-    -- Deincrement the number of windows in the master area
+-- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
     -- Toggle the status bar gap
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
     -- See also the statusBar function from Hooks.DynamicLog.
     --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+    , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
-    -- Quit xmonad
+-- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
-    -- Restart xmonad
+-- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
 
-    -- Run xmessage with a summary of the default keybindings (useful for beginners)
+-- Run xmessage with a summary of the default keybindings (useful for beginners)
     , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
-    
-    -- controll volume
+
+-- displayct
+    , ((modm .|. shiftMask, xK_d), spawn "displayctl menu")
+    , ((modm .|. shiftMask, xK_a), spawn "displayctl auto")
+
+-- controll volume
     , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
     , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%")
     , ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
     
-    -- controll Brightness
+-- controll Brightness
     , ((0, xF86XK_MonBrightnessDown), spawn "lux -s 5%")
     , ((0, xF86XK_MonBrightnessUp), spawn "lux -a 5%")
     ]
@@ -176,11 +197,20 @@ myLogHook = return ()
 
 -- Startup hook
 myStartupHook = do
+    spawn "killall conky"   -- kill current conky on each restart
+    spawn "killall trayer"  -- kill current trayer on each restart
+
+    spawnOnce "picom"
+	spawnOnce "dunst"
+    spawnOnce "nm-applet"
+    spawnOnce "volumeicon"
+    spawnOnce "kdeconnect-indicator"    
+    spawn ("sleep 2 && conky -c $HOME/.conky/nord-01.conkyrc")
+    spawn ("sleep 2 && trayer --edge top --align right --tint 0x2E3440 --width 15 --monitor 0 --transparent true --alpha 0 --height 28")
+
     spawnOnce "nitrogen --restore &"
-    spawnOnce "compton &"
 
 main = do
-  xmproc <- spawnPipe "~/.fehbg"
   xmproc <- spawnPipe "picom"
   xmproc <- spawnPipe "xmobar -x 0 /home/jonas/.config/xmobar/xmobarrc"
   xmproc <- spawnPipe "xmobar -x 1 /home/jonas/.config/xmobar/xmobarrc"
@@ -204,7 +234,7 @@ defaults = def {
         mouseBindings      = myMouseBindings,
 
       -- hooks, layouts
-        layoutHook         = spacingWithEdge 10 $ myLayout,
+        layoutHook         = spacingWithEdge 4 $ myLayout,
         manageHook         = myManageHook,
         handleEventHook    = myEventHook,
         logHook            = myLogHook,
@@ -217,7 +247,7 @@ help = unlines ["The default modifier key is 'alt'. Default keybindings:",
     "",
     "-- launching and killing programs",
     "mod-Shift-Enter  Launch terminal",
-    "mod-p            Launch dmenu",
+    "mod-p            Launch rofi",
     "mod-Shift-c      Close/kill the focused window",
     "mod-Space        Rotate through the available layout algorithms",
     "mod-Shift-Space  Reset the layouts on the current workSpace to default",
